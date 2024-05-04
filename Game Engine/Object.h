dc9455197648b959c35a19587844e51bc557ad14
walkingJo <86416726+walkingJo@users.noreply.github.com>
2024-05-04 12:32:18 +0900

    Add files via upload

--- Game Engine/Object.h
+++ Game Engine/Object.h
@@ -6,30 +6,65 @@
 
 class Object {
 public:
-	/*
-	* game
-	* 
-	* location
-	* direction
-	* rotation
-	* 
-	* ----
-	* 
-	* move
-	* moveTo
-	* 
-	* rotate
-	* totateX
-	* totateY
-	* totateZ
-	* 
-	* lookAt
-	* 
-	* basisConversion...?
-	*/
+	Object() {
+		location = Vector3::Zero;
+		direction = Vector3::UnitY;
+		rotation = 0.0f;
+
+		*model = Model::Cube;
+	}
+	Object(Model* modelData) {
+		*this = Object();
+		setModel(modelData);
+	}
+	void setModel(Model* modelData) {
+		model = modelData;
+	}
+
+	void move(const Vector3& movement) {
+		location += movement;
+		model->move(movement);
+	}
+	void moveTo(const Vector3& location) {
+		this->location = location;
+		model->moveTo(location);
+	}
+	void rotate(const Vector3& axis, float radian) {
+		direction.rotate(axis, radian);
+		model->rotate(axis, radian);
+	}
+	void rotateX(float radian) {
+		rotate(Vector3::UnitX, radian);
+	}
+	void rotateY(float radian) {
+		rotate(Vector3::UnitY, radian);
+	}
+	void rotateZ(float radian) {
+		rotate(Vector3::UnitZ, radian);
+	}
+	void rotate(const Vector3& axis, float radian, const Vector3& rotateCentor) {
+		rotate(axis, radian);
+		this->location = (this->location - rotateCentor).rotate(axis, radian) + rotateCentor;
+	}
+
+	//basisConversionTo //>> 얘는 어디다 넣는게 맞는건가
+	void lookAt(Vector3 target) {
+		Vector3 targetDirection = target - location; targetDirection.normalize();
+		//단순히 하나의 축으로 회전하는 것이 아닌, z축과 xy으로만 회전할 수 있도록 구현
+		//  1. direction과 targetDirection의 xy 성분만을 이용해 회전
+		//  2. 1번 더 회전
+	}
+	
 	void update() {
 		return;
 	}
+
+private:
+	Vector3 location;
+	Vector3 direction;
+	float rotation;
+
+	Model* model;
 };
 
 #endif // !OBJECT_H
